bffe1c16ced29de01ce37a070f6b73f625c38ca8
Serge <sergio_ld@hotmail.com>
2022-11-02 14:24:56 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int cantidadDeNotasACargo = 0;
+double promediodeNotas = 0;
+double sumaDeNotas = 0;
+
+Console.WriteLine("Ingresa la cantidad de notas a cargar: ");
+cantidadDeNotasACargo = int.Parse(Console.ReadLine());
+
+for (int i = 1; i<=cantidadDeNotasACargo; i++)
+{
+    Console.WriteLine("Ingrese una nota: ");
+    double nota = double.Parse(Console.ReadLine());
+    sumaDeNotas = sumaDeNotas + nota;
+}
+
+promediodeNotas = sumaDeNotas / cantidadDeNotasACargo;
+Console.WriteLine("El promedio de las notas cargadas es de {0}", promediodeNotas);
